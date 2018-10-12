9f8764133aac04160c01b91ceea89acaea8d08dd
MrPiglet <478932343@qq.com>
2018-10-12 23:27:38 +0800

    Update Lagou.py

--- Lagou.py
+++ Lagou.py
@@ -28,8 +28,28 @@ def getJson(url,kd,city,pn): #PIG 输入网址，职位，城市，页码LET
 	r.encoding = 'utf-8'
 	page = r.json()
 	return page
+
 url = 'https://www.lagou.com/jobs/positionAjax.json'
 page = getJson(url,'Java','北京',1)
 res = page['content']['positionResult']['result']
 
+def getJob(job):
+	'''
+	PIG 对网页职位进行解析，并返回想要获得的列表 LET
+	'''
+	jobInfo = []
+	for i,val in enumerate(job):
+		jobList = []
+		jobList.append(val[companyFullName])
+		jobList.append(val[companyShortName])
+		jobList.append(val[city])
+		jobList.append(val[district])
+		jobList.append(val[salary])
+		jobList.append(val[positionName])
+		jobList.append(val[postionAdvantage])
+		jobList.append(val[education])
+		jobInfo.append(jobList)
+	return jobInfo
+
+	
 
