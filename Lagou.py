408026174776a285efe2857c4e4fdbfbda2bb47b
MrPiglet <478932343@qq.com>
2018-10-12 14:48:14 +0800

    Update Lagou.py

--- Lagou.py
+++ Lagou.py
@@ -10,19 +10,19 @@ def getJson(url,kd,city,pn): #PIG 输入网址，职位，城市，页码LET
 	myParams = {
 			'px':'default',
 			'city':City,
-			'needAddtionalResult':'false',
+			'needAddtionalResult':'false'
 			}
 	myHeaders = {
 			'Accept':'application/json, text/javascript, */*; q=0.01',
 			'Connection':'keep-alive',
 			'Host':'www.lagou.com',
 			'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
-			'Referer':'https://www.lagou.com/jobs/list_'+Kd+'?px=default&city=]'+City,
+			'Referer':'https://www.lagou.com/jobs/list_'+Kd+'?px=default&city=]'+City
 			}
 	myData = {
 			'first':'true',
 			'pn':pn,
-			'kd':kd,
+			'kd':kd
 			}
 	r = requests.post(url,params = myParams,headers = myHeaders,data = myData)
 	r.encoding = 'utf-8'
