a8365d561d0f7359cf5216ac197435213cc1ebea
MrPiglet <478932343@qq.com>
2018-10-16 21:11:50 +0800

    Update Lagou.py

--- Lagou.py
+++ Lagou.py
@@ -36,7 +36,8 @@ def parseInfo(job):
 	PIG 对网页职位进行解析，并返回想要获得的列表 LET
 	'''
 	jobInfo = []
-	for i,val in enumerate(job): #PIG 获取想要得到的数据
+	res = job['content']['positionResult']['result'] 
+	for i,val in enumerate(res): #PIG 获取想要得到的数据
 		jobList = []
 		jobList.append(val['companyFullName'])
 		jobList.append(val['companyShortName'])
@@ -55,14 +56,13 @@ def main():
 	num = 2 #PIG 所要获取的页数
 	for i in range(1,num+1):
 		page = getInfo(url,'Java','北京',i) #PIG 写入想要获取的职位信息以及职位所在地
-		res = page['content']['positionResult']['result'] #PIG 所有数据都储存在这个位置里面
-		pageInfo = parseInfo(res)
+		pageInfo = parseInfo(page)
 		totalInfo += pageInfo #PIG 将所有职位信息集合在一起
 		print('第{}页处理完成'.format(i))
 		time.sleep(10)
 	df = pd.DataFrame(totalInfo,columns = ['公司全称','公司简称','所在城市','所在地区',
 											'职位名称','薪资','职位福利','学历要求'])
-	df.to_csv('lagou.csv',index = False)
+	df.to_csv('lagou.csv',index = False,encoding = 'utf-8-sig') #PIG 不加这个格式的encoding会导致打开文件时出现乱码
 	print('文件保存成功')
 
 if __name__ == '__main__':
